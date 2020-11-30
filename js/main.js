6249315ca82b779bd6ef3453175103ea5e87d310
Ivan <delfatti.ivan@gmail.com>
2020-11-30 14:41:56 +0100

    Add small cards
    Add dynamically small cards

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@
 
      data: {
 
+        // HEADER / NABAR
+
         // POPULATING NAVBAR LIST
         navbarList: [
             {
@@ -41,6 +43,34 @@
             },
             {
                 icon: 'fa-bars',
+            },
+        ],
+
+        // MAIN SMALL CARDS
+        smCards: [
+            {
+                image: '1',
+                title: 'Languages'
+            },
+            {
+                image: '2',
+                title: 'Software'
+            },
+            {
+                image: '3',
+                title: 'Business'
+            },
+            {
+                image: '4',
+                title: 'Chemistry'
+            },
+            {
+                image: '5',
+                title: 'Science'
+            },
+            {
+                image: '6',
+                title: 'DIT&Craft'
             },
         ],
 
