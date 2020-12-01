b764cd2c1a7fa98c4743ae9ee9a54d23fb7257bd
Ivan <delfatti.ivan@gmail.com>
2020-12-01 16:17:54 +0100

    Add slider images

--- js/main.js
+++ js/main.js
@@ -57,15 +57,15 @@
                 button: 'REGISTER NOW',
             },
             {
-                image: 'h5-slide-3-background',
-                title: 'Contemporary Ideas2',
-                text: ' ipsam iste in esse quo eos numquam ipsa ullam repellendus, alias praesentium quis atque ex ducimus deserunt illum!',
+                image: 'h5-slide-2-background',
+                title: 'Contemporary',
+                text: ' ipsam iste in esse quo eos numquam ipsaesse quo eos numquam ipsaesse quo eos numquam ipsa ullam repellendus, alias praesentium quis atque ex ducimus deserunt illum!',
                 button: 'REGISTER NOW',
             },
             {
-                image: 'h5-slide-3-background',
-                title: 'Contemporary Ideas3',
-                text: 'Lorem ipsum dolor, elit.Lorem ipsum dolor, elit.Lorem ipsum dolor, elit.Lorem ipsum dolor, elit. Sed odio ipsam iste in esse quo eos numquam ipsa ullam repellendus, alias praesentium quis atque ex ducimus deserunt illum!',
+                image: 'h5-slide-1-background',
+                title: 'Ideas',
+                text: 'Lorem ipsum dolor, elit.Lorem ipsum dolor, elit. Sed odio ipsam iste in esse quo eos numquam ipsa ullam repellendus, alias praesentium quis atque ex ducimus deserunt illum!',
                 button: 'REGISTER NOW',
             },
         ],
@@ -108,15 +108,15 @@
                 type: 'STUDENT',
             },
             {
-                image: 'testimonials-standard-2',
+                image: 'testimonials-standard-1',
                 text: 'consectetur adipisicing elit. Ducimus obcaecati qui illum deleniti iste repudiandae dolor, sit amet consectetur psam ea libero iste in esse quo eos numquam ipsa ullam repellendus, alias praesentium quis atque ex ducimus deserunt illum!',
-                name: 'Johan Collins2',
+                name: 'Arden Cho',
                 type: 'TEACHER',
             },
             {
-                image: 'testimonials-standard-2',
+                image: 'testimonials-standard-3',
                 text: 'consectetur adipisicing elit. Ducimus obcaecati qui illum deleniti iste repudiandae dolor, sit amet consectetur psam ea libero iste in esse quo eos numquam ipsa ullam repellendus, alias praesentium quis atque ex ducimus deserunt illum!',
-                name: 'Johan Collins3',
+                name: 'Scott Rossi',
                 type: 'DESIGNER',
             },
         ],
@@ -134,11 +134,11 @@
                     'The trusted name for specialized training.',
                     'People teach. People learn. This in whewe they connect.'
                 ],
-                image: '',
+                image: 'h12-tabs-icon-1',
             },
             {
                 name: 'Degree Programme',
-                title: 'Learn Ciaone',
+                title: 'Degree',
                 text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta!',
                 list: [ 
                     'We enrich throught learning.',
@@ -146,11 +146,11 @@
                     'The trusted name for specialized training.',
                     'People teach. People learn. This in whewe they connect.'
                 ],
-                image: '',
+                image: 'h12-tabs-icon-2',
             },
             {
                 name: 'Career Achievements',
-                title: 'Learn Prova',
+                title: 'Achievements',
                 text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta!',
                 list: [ 
                     'We enrich throught learning.',
@@ -158,11 +158,11 @@
                     'The trusted name for specialized training.',
                     'People teach. People learn. This in whewe they connect.'
                 ],
-                image: '',
+                image: 'h12-tabs-icon-3',
             },
             {
                 name: 'Personal Managment',
-                title: 'Learn Possibilities',
+                title: 'Managment',
                 text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta!',
                 list: [ 
                     'We enrich throught learning.',
@@ -170,21 +170,21 @@
                     'The trusted name for specialized training.',
                     'People teach. People learn. This in whewe they connect.'
                 ],
-                image: '',
+                image: 'h12-tabs-icon-4',
             },
             {
                 name: 'Steps To Success',
-                title: 'Learn Possibilities',
+                title: 'Steps To Success',
                 text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta!',
                 list: [ 
                     'We enrich throught learning.',
                     'People teach. People learn. This in whewe they connect.'
                 ],
-                image: '',
+                image: 'h12-tabs-icon-5',
             },
             {
                 name: 'Knowledge Transfer',
-                title: 'Learn Possibilities',
+                title: 'Knowledge',
                 text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, iusto? Nostrum, ducimus similique amet hic sunt at velit totam deserunt perspiciatis, repellendus minus suscipit tempore ullam numquam blanditiis cum dicta!',
                 list: [ 
                     'We enrich throught learning.',
@@ -192,7 +192,7 @@
                     'The trusted name for specialized training.',
                     'People teach. People learn. This in whewe they connect.'
                 ],
-                image: '',
+                image: 'h12-tabs-icon-6',
             },
         ],
 
@@ -202,7 +202,7 @@
             {
                 slider: [
                     {
-                        image: './img/main/courses/popular-courses/course-5-f-img',
+                        image: 'course-5-f-img',
                         title: 'Android Developer',
                         author: 'David Sanders',
                         price: 'free',
@@ -219,7 +219,7 @@
                         ],
                     },
                     {
-                        image: './img/main/courses/popular-courses/course-6-f-img',
+                        image: 'course-6-f-img',
                         title: 'Web Designer',
                         author: 'Jennefer Powell',
                         price: 'free',
@@ -236,7 +236,7 @@
                         ],
                     },
                     {
-                        image: './img/main/courses/popular-courses/course-12-f-img',
+                        image: 'course-12-f-img',
                         title: 'Financial Modeling',
                         author: 'Edward Bowman',
                         price: '$20',
@@ -257,7 +257,7 @@
             {
                 slider: [
                     {
-                        image: './img/main/courses/popular-courses/course-5-f-img',
+                        image: 'course-4-f-img',
                         title: 'Web Developer',
                         author: 'Andrea Sancez',
                         price: '$16',
@@ -278,24 +278,24 @@
                         ],
                     },
                     {
-                        image: './img/main/courses/popular-courses/course-6-f-img',
-                        title: 'Web Designer',
-                        author: 'Jennefer Powell',
+                        image: 'course-8-f-img',
+                        title: 'Photographer',
+                        author: 'Yennefer Bianchi',
                         price: '$20',
                         text: 'Quidem natus. Soluta id, provident, provident hic vero ut numquam assumenda fuga autem exercitationem quae!',
                         tags: [
                             {
                                 icon: 'fa-user',
-                                text: '1',
+                                text: '12',
                             },
                             {
                                 icon: 'fa-tag',
-                                text: 'PROGRAMMING',
+                                text: 'BUSINESS',
                             }
                         ],
                     },
                     {
-                        image: './img/main/courses/popular-courses/course-12-f-img',
+                        image: 'course-10-f-img',
                         title: 'Designer',
                         author: 'Edward',
                         price: '$90',
@@ -316,26 +316,26 @@
             {
                 slider: [
                     {
-                        image: './img/main/courses/popular-courses/course-5-f-img',
-                        title: 'Android Developer',
-                        author: 'David Sanders',
+                        image: 'course-1-f-img',
+                        title: 'English Teacher',
+                        author: 'Samanta Sancez',
                         price: '$89',
                         text: 'Quidem natus. Soluta id, provident, provident hic vero ut numquam assumenda fuga autem exercitationem quae!',
                         tags: [
                             {
                                 icon: 'fa-user',
-                                text: '1',
+                                text: '7',
                             },
                             {
                                 icon: 'fa-tag',
-                                text: 'PROGRAMMING',
+                                text: 'LANGUAGE',
                             }
                         ],
                     },
                     {
-                        image: './img/main/courses/popular-courses/course-6-f-img',
-                        title: 'Web Designer',
-                        author: 'Jennefer Powell',
+                        image: 'course-2-f-img',
+                        title: 'Graphic',
+                        author: 'Gerard Powell',
                         price: '$67',
                         text: 'Quidem natus. Soluta id, provident, provident hic vero ut numquam assumenda fuga autem exercitationem quae!',
                         tags: [
@@ -345,14 +345,14 @@
                             },
                             {
                                 icon: 'fa-tag',
-                                text: 'PROGRAMMING',
+                                text: 'DESIGN',
                             }
                         ],
                     },
                     {
-                        image: './img/main/courses/popular-courses/course-12-f-img',
-                        title: 'Financial Modeling',
-                        author: 'Edward Bowman',
+                        image: 'course-3-f-img',
+                        title: 'Math',
+                        author: 'Bowman',
                         price: '$20',
                         text: 'Quidem natus. Soluta id, provident, provident hic vero ut numquam assumenda fuga autem exercitationem quae!',
                         tags: [
